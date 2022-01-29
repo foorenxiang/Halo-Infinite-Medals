475748433f87ae70dec85249409b065dbc94c157
Ren Xiang <foorenxiang@gmail.com>
2022-01-29 11:10:01 +0800

    added download and saving optimisations

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from pprint import pprint
 import requests
 from pathlib import Path
 from get_stats import get_stats
+import logging
+from functools import lru_cache
+
+logger = logging.getLogger(__name__)
+MEDAL_CACHE_FOLDER = Path("./.medal_cache")
 
 
 class PathStr(str):
@@ -35,40 +39,74 @@ def _create_medal_folder(medals_folder: PathStr):
     Path(medals_folder).mkdir(exist_ok=True)
 
 
+@lru_cache()
+def get_medal_image_data(medal_image_url):
+    medal_filename = Path(medal_image_url).name
+    medal_cache_file: Path = MEDAL_CACHE_FOLDER / medal_filename
+    if medal_cache_file.exists():
+        medal_image_data = open(medal_cache_file, "rb").read()
+    else:
+        res = requests.get(medal_image_url)
+        if 200 <= res.status_code < 300:
+            medal_image_data = res.content
+        else:
+            logger.warning(f"Failed to get medal for {medal_image_url}")
+            return
+        with open(medal_cache_file, "wb") as fp:
+            fp.write(medal_image_data)
+    return medal_image_data
+
+
 def _download_medals(medal_stats: dict, medals_folder: PathStr):
     print("")
     for medal_descriptor in medal_stats:
         medal_image_url = medal_descriptor["image_urls"]["large"]
         medal_count = medal_descriptor["count"]
-        res = requests.get(medal_image_url)
-        if 200 <= res.status_code < 300:
-            medal_name = str(Path(medal_image_url).stem)
-            print(
-                f"Awarding {medal_count} {medal_name} medal{'s' if medal_count > 1 else ''}"
+        medal_name = str(Path(medal_image_url).stem)
+        medal_image_data = get_medal_image_data(medal_image_url)
+
+        if medal_image_data:
+            medal_count = medal_descriptor["count"]
+            new_medal_count = _create_medal_images(
+                medals_folder,
+                medal_count,
+                medal_image_url,
+                medal_image_data,
             )
-            _create_medal_images(medals_folder, medal_descriptor, medal_image_url, res)
+            if new_medal_count:
+                print(
+                    f"Awarding {new_medal_count} new {medal_name} medal{'s' if medal_count > 1 else ''}"
+                )
         else:
-            print("Failed to get", medal_image_url)
+            logger.warning("Failed to award medal_name")
 
 
 def _create_medal_images(
     medals_folder: str,
-    medal_descriptor: dict,
+    medal_count: int,
     medal_image_url: str,
-    res: requests.Response,
+    medal_image_data: bytes,
 ):
-    for idx in range(medal_descriptor["count"]):
-        medal_file_name = f"./{medals_folder}/{Path(medal_image_url).stem}_{idx+1}{Path(medal_image_url).suffix}"
+    medal_path_stem = Path(medal_image_url).stem
+    medal_path_suffix = Path(medal_image_url).suffix
+    new_medal_count = 0
+    for idx in range(medal_count):
+        medal_file_name = (
+            f"{medals_folder}/{medal_path_stem}_{idx+1}{medal_path_suffix}"
+        )
         if not Path(medal_file_name).exists():
+            new_medal_count += 1
             with open(
                 medal_file_name,
                 "wb",
             ) as fp:
-                fp.write(res.content)
+                fp.write(medal_image_data)
+    return new_medal_count
 
 
 def save_medals(spartan_id: str, medal_stats: dict):
     medals_folder = PathStr(f"./{spartan_id}")
+    MEDAL_CACHE_FOLDER.mkdir(exist_ok=True)
     _create_medal_folder(medals_folder)
     _download_medals(medal_stats, medals_folder)
 
