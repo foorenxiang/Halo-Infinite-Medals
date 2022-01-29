1d9f5692eeda9647d903155aea7d4c2c0324057c
Ren Xiang <foorenxiang@gmail.com>
2022-01-29 09:44:51 +0800

    add fastapi for serving medals

---
+++ serve.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from main import get_spartan_medal_stats
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/spartan/{spartan_id}")
+def spartan_stats(spartan_id: str):
+    medals_list = get_spartan_medal_stats(spartan_id)
+    response = {spartan_id: medals_list}
+    return response
